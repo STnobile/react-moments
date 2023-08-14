06447edd232658320bc7f63cab209a4dc1045e08
STnobile <stnobile0@gmail.com>
2023-08-14 18:37:14 +0000

    Add Nav bar and rout, also create sign in page and form

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+
+const SignUpForm = () => {
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-auto py-2 p-md-2" md={6}>
+                <Container className={`${appStyles.Content} p-4 `}>
+                    <h1 className={styles.Header}>sign up</h1>
+
+                    {/* add your form here */}
+                    <Form>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">username</Form.Label>
+                            <Form.Control type="text" placeholder="User Name" />
+                        </Form.Group>
+
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" name="passwor"/>
+                        </Form.Group>
+        
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+
+                </Container>
+                <Container className={`mt-3 ${appStyles.Content}`}>
+                    <Link className={styles.Link} to="/signin">
+                        Already have an account? <span>Sign in</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col
+                md={6}
+                className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+            >
+                <Image
+                    className={`${appStyles.FillerImage}`}
+                    src={
+                        "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
+                    }
+                />
+            </Col>
+        </Row>
+    );
+};
+
+export default SignUpForm;
